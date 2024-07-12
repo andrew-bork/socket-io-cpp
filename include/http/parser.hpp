537aa7a0543b7666d14f71f36a3a5edc861a8eb1
Andrew Lin <bork.andrew.ryan@gmail.com>
2024-07-12 13:11:45 -0700

    Replaced http-parser with llhttp

--- include/http/parser.hpp
+++ include/http/parser.hpp
@@ -30,70 +30,12 @@ namespace http {
         void close();
     };
 
-    struct version_parser {
-        enum {
-            EXPECT_H,
-            EXPECT_T,
-            EXPECT_TT,
-            EXPECT_P,
-            EXPECT_SLASH,
-            EXPECT_MAJOR_DIGIT,
-            EXPECT_MAJOR_DIGIT_OR_DOT,
-            EXPECT_MINOR_DIGIT,
-            EXPECT_MINOR_DIGIT_OR_SPACE,
-            DONE
-        } state = EXPECT_H;
-
-        std::string token = "";
-        int major = 0;
-        int minor = 0;
-
-        bool parse(char c);
-    };
-
-    struct status_code_parser {
-        enum {
-            EXPECT_DIGIT,
-            EXPECT_DIGIT_OR_SPACE,
-            DONE
-        } state = EXPECT_DIGIT;
-
-        std::string token = "";
-        int status_code = 0;
-
-        bool parse(char c);
-    };
-
-    struct status_reason_parser {
-        enum {
-            EXPECT_TEXT_OR_CR,
-            EXPECT_LF,
-            DONE
-        } state = EXPECT_TEXT_OR_CR;
-
-        std::string reason = "";
-        bool parse(char c);
-    };
-
     struct request_parser {
-        enum State {
-            REQUEST_LINE,
-            HEADERS,
-            BODY,
-            END,
-            CHUNK_END,
-            DONE
-        } state = REQUEST_LINE;
-
         http::request request;
 
-        bool chunked = false;
-
-
-        size_t length_waiting_for = 0;
         http::stream body;
 
-        std::string unparsed = "";
+        request_parser();
         bool is_header_done();
         bool is_done();
         bool parse(const std::string& str);
@@ -101,32 +43,9 @@ namespace http {
 
     struct response_parser {
         http::response response;
-
-        enum {
-            // RESPONSE_LINE,
-            // HEADERS,
-            // BODY,
-            // END,
-            // CHUNK_END,
-            // DONE
-            VERSION, STATUS_CODE, STATUS_REASON, HEADER, BODY, DONE
-            // VERSION, STATUS_REASON, STATUS_CODE, HEADER_OR_BODY, HEADER_KEY, HEADER_VALUE_WHITESPACE, HEADER_VALUE, BODY, NONE, FINISHED
-        } state = VERSION; // = RESPONSE_LINE;
-        // bool last_char_was_cr = false;
-
-
-        size_t length_waiting_for = 0;
-        bool chunked = false;
-        
-        std::string unparsed = "";
-
         http::stream body;
 
+        response_parser();
         bool parse(const std::string& response_string);
-        private:
-            version_parser version_parser;
-            status_code_parser status_code_parser;
-            status_reason_parser status_reason_parser;
-
     };
 }
